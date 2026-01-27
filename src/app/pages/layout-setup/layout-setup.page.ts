e47da0f7843b691858351ee7691c5e3fbaf7300e
praveen9248 <praveenreddy9248@gmail.com>
2026-01-27 18:43:29 +0530

    connect build

--- src/app/pages/layout-setup/layout-setup.page.ts
+++ src/app/pages/layout-setup/layout-setup.page.ts
@@ -1,5 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { PageFlowService } from 'src/app/services/pageFlow/page-flow-service';
+import {
+  ConnectionInfo,
+  LanTransfer,
+  ServerInfo,
+} from 'capacitor-lan-transfer';
 
 @Component({
   selector: 'app-layout-setup',
@@ -9,9 +14,24 @@ import { PageFlowService } from 'src/app/services/pageFlow/page-flow-service';
 })
 export class LayoutSetupPage {
   pageFlowService = inject(PageFlowService);
+  serverStatus = signal<any>(null);
 
-  deviceCredentials: string = JSON.stringify({
-    deviceId: 'LCD-SOLUM-123',
-    passKey: 'SOLUM-IND-KA',
-  });
+  async onStartServer() {
+    const info = await LanTransfer.initialize({
+      role: 'server',
+      port: 0,
+      receiveMode: 'file',
+      receiveFileName: `incoming_${Date.now()}.json`,
+    });
+    this.serverStatus.set(info);
+
+    await LanTransfer.addListener('receiveComplete', (e) => {
+      this.serverStatus.set(e.filePath);
+    });
+  }
+
+  async onStopServer() {
+    await LanTransfer.stopServer();
+    this.serverStatus.set(null);
+  }
 }
