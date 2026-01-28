0cd5ff58ef16a5d93fb20f3e4b21a8e173f87300
praveen9248 <praveenreddy9248@gmail.com>
2026-01-28 18:10:08 +0530

    rendered log of connection

--- src/app/pages/layout-setup/layout-setup.page.ts
+++ src/app/pages/layout-setup/layout-setup.page.ts
@@ -1,9 +1,11 @@
 import { Component, inject, signal } from '@angular/core';
 import { PageFlowService } from 'src/app/services/pageFlow/page-flow-service';
 import {
-  ConnectionInfo,
+  ErrorEvent,
   LanTransfer,
-  ServerInfo,
+  ProgressEvent,
+  ReceiveCompleteEvent,
+  StatusEvent,
 } from 'capacitor-lan-transfer';
 
 @Component({
@@ -14,24 +16,112 @@ import {
 })
 export class LayoutSetupPage {
   pageFlowService = inject(PageFlowService);
-  serverStatus = signal<any>(null);
+  serverStatus = signal<boolean>(false);
+  serverInfo = signal<any>(null);
+  uiConnectionUpdate = signal<any[]>([]);
+  receiveProgress = signal<number>(0);
+
+  private statusListener?: { remove: () => Promise<void> };
+  private progressListener?: { remove: () => Promise<void> };
+  private receiveListener?: { remove: () => Promise<void> };
+  private errorListener?: { remove: () => Promise<void> };
+
+  async ngOnInit() {
+    this.registerEventListeners();
+  }
+
+  async registerEventListeners() {
+    this.statusListener = await LanTransfer.addListener(
+      'status',
+      (e: StatusEvent) => {
+        if (e.role !== 'server') return;
+
+        switch (e.status) {
+          case 'server_started':
+            this.serverStatus.set(true);
+            this.pushUIMessage(e.status);
+            break;
+
+          case 'server_stopped':
+            this.serverStatus.set(false);
+            this.pushUIMessage(e.status);
+            break;
+
+          case 'connection_accepted':
+            this.receiveProgress.set(0);
+            this.pushUIMessage('Client Connected');
+            break;
+
+          case 'client_disconnected':
+            this.pushUIMessage('Client Disconnected');
+            break;
+
+          case 'connection_closed':
+            this.pushUIMessage('Connection Closed');
+            break;
+        }
+      },
+    );
+
+    this.progressListener = await LanTransfer.addListener(
+      'progress',
+      (e: ProgressEvent) => {
+        if (e.role !== 'server' || e.direction !== 'receive') return;
+
+        this.receiveProgress.set(e.percent);
+      },
+    );
+
+    this.receiveListener = await LanTransfer.addListener(
+      'receiveComplete',
+      (e: ReceiveCompleteEvent) => {
+        if (e.role !== 'server') return;
+        this.receiveProgress.set(100);
+        this.pushUIMessage(`File received (${e.totalBytes}) bytes`);
+      },
+    );
+
+    this.errorListener = await LanTransfer.addListener(
+      'error',
+      (e: ErrorEvent) => {
+        if (
+          e.role !== 'server' &&
+          e.code === 'SERVER_ERROR' &&
+          e.message?.includes('Socket')
+        )
+          return;
+
+        this.pushUIMessage(`Error : ${e.message}`);
+      },
+    );
+  }
 
   async onStartServer() {
-    const info = await LanTransfer.initialize({
+    this.uiConnectionUpdate.set([]);
+    await LanTransfer.initialize({
       role: 'server',
       port: 0,
       receiveMode: 'file',
-      receiveFileName: `incoming_${Date.now()}.json`,
+      receiveFileName: 'configuration.json',
     });
-    this.serverStatus.set(info);
-
-    await LanTransfer.addListener('receiveComplete', (e) => {
-      this.serverStatus.set(e.filePath);
-    });
+    let info = await LanTransfer.getServerInfo();
+    this.serverInfo.set(info);
   }
 
   async onStopServer() {
     await LanTransfer.stopServer();
-    this.serverStatus.set(null);
+    this.serverInfo.set(null);
+  }
+
+  pushUIMessage(message: any) {
+    if (!message) return;
+    this.uiConnectionUpdate.update((prev) => [...prev, message]);
+  }
+
+  async ngOnDestroy() {
+    await this.statusListener?.remove();
+    await this.errorListener?.remove();
+    await this.progressListener?.remove();
+    await this.receiveListener?.remove();
   }
 }
